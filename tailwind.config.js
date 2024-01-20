cb88567bd6a29ad5ad3dfe472de08bbe7ae32ab9
unknown <hafsaimran410@gmail.com>
2024-01-20 15:37:14 +0500

    Gloria Food

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        customWhite: 'rgba(255, 255, 255, 0.2)', 
+        customBlack: 'rgba(0, 0, 0, 0.4)',
+        customOrange: '#F08B18',
+        customGray: '#4C4C4C',
+        customBlue: '#007BFF',
+        customBlack2: '#999999',
+        customGrayBlue: '#6C757D',
+        customWhite2: 'rgba(255, 255, 255, 0.08)',
+        customWhite3: 'rgba(255, 255, 255, 0.6)',
+        black2: '#333C44',
+        customWhite30: 'rgba(255, 255, 255, 0.3)',
+      },
+    },
+  },
+  plugins: [],
+}
+
